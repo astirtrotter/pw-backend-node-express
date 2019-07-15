754d6569cc0ba8a198576639521b761fe1d104e7
prodev9999 <prodev9999@gmail.com>
2019-07-15 19:00:45 +0200

    Usage of db connection in express app

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = require('./config/properties').PORT;
+const db = require('./config/database');
+
+// call the database connectivity function
+db();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}`));
