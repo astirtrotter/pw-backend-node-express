61d7bc9691708ba5c8e2e34d2b245810a692951a
prodev9999 <prodev9999@gmail.com>
2019-07-15 18:53:43 +0200

    Added database connection handler

---
+++ config/database.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+const dbURL = require('./properties').DB;
+
+const connected = chalk.bold.cyan;
+const error = chalk.bold.yellow;
+const disconnected = chalk.bold.red;
+const termination = chalk.bold.magenta;
+
+module.exports = () => {
+  mongoose.connect(dbURL);
+
+  mongoose.connection.on('connected', () => {
+    console.log(connected('Mongoose default connection is open to ', dbURL));
+  });
+  mongoose.connection.on('error', err => {
+    console.log(error('Mongoose default connection has occured ' + err + ' error'));
+  });
+  mongoose.connection.on('disconnected', () => {
+    console.log(disconnected('Mongoose default connection is disconnected'));
+  });
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log(termination('Mongoose default connection is disconnected due to application termination'));
+      process.exit(0);
+    });
+  });
+};
