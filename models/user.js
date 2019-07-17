cb6b519091e637fbd11d232722d55d4654ace6aa
prodev9999 <prodev9999@gmail.com>
2019-07-17 23:30:01 +0200

    Updated user model

--- models/user.js
+++ models/user.js
@@ -7,44 +7,46 @@ const schema = new mongoose.Schema({
     required: 'Email is required',
     match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
-  name: {
-    type: String,
-    trim: true,
-    required: 'Name is required',
-    match: [/^[\w\s]+$/, 'Please fill a valid name']
-  },
-  about: {
-    type: String,
-    trim: true,
-    required: 'About is required'
-  },
   salt_password: String,
-  location: {
-    type: String,
-    trim: true,
-    required: 'Location is required',
-    match: [/^\w+, \w+$/, 'Please fill a valid location']
+  profile: {
+    name: {
+      type: String,
+      trim: true,
+      required: 'Name is required',
+      match: [/^[\w\s]+$/, 'Please fill a valid name']
+    },
+    overview: {
+      type: String,
+      trim: true,
+      required: 'About is required'
+    },
+    location: {
+      type: String,
+      trim: true,
+      required: 'Location is required',
+      match: [/^\w+, \w+$/, 'Please fill a valid location']
+    },
+    skills: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Skills'
+    }],
+    portfolios: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Portfolios'
+    }],
+    clients: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Clients'
+    }],
+    testimontials: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Testimonials'
+    }]
   },
   meta: {
     admin: {type: Boolean, default: false},
     lock: {type: Boolean, default: false}
-  },
-  skills: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Skills'
-  }],
-  portfolios: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Portfolios'
-  }],
-  clients: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Clients'
-  }],
-  testimontials: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Testimonials'
-  }]
+  }
 }, {
   timestamps: true
 });
