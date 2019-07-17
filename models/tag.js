e573467f3811ce721d29716f275917bc77632d85
prodev9999 <prodev9999@gmail.com>
2019-07-17 23:08:41 +0200

    Updated tag model

--- models/tag.js
+++ models/tag.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
-  name: {type: String, unique: true, required: true},
-  color: {type:Number, required: true, default: 0},
+  name: {
+    type: String,
+    unique: true,
+    required: 'Name is required',
+    trim: true,
+    match: [/^[a-z_A-Z-.]+$/, 'Please fill a valid tag name']
+  },
+  color: {
+    type:Number,
+    default: 0,
+    max: 0xFFFFFF
+  },
   description: String
 }, {
   timestamps: true
