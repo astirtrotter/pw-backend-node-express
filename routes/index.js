56236e5015b5898458d60735c366f4abdb116ec4
prodev9999 <prodev9999@gmail.com>
2019-07-18 00:01:32 +0200

    Added error pages

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 const tagRoute = require('./tag');
 
 module.exports = router => {
+  // router.use((req, res, next) => {
+  //   next();
+  // });
+
   router.get('/', (req, res) => {
     res.render('index', {title: 'Admin Panel'})
   });
 
   tagRoute(router);
+
+  router.use((req, res) => {
+    res.render('404', {url: req.url});
+    // res.render('500', {error: new Error('message error')});
+  });
 
   return router;
 };
