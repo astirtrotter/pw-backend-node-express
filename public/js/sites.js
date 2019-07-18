ab7c27d90b0e52171dd0e84ac1123b06a676acd9
prodev9999 <prodev9999@gmail.com>
2019-07-18 20:00:24 +0200

    Minor update

--- public/js/sites.js
+++ public/js/sites.js
@@ -1,10 +1,10 @@
-$(function(){
-  var current = location.pathname;
-  $('ul li a').each(function(){
-    var $this = $(this);
+$(function () {
+  let current = location.pathname;
+  $('ul li a').each(function () {
+    let $this = $(this);
     // if the current path is like this link, make it active
-    if($this.attr('href').indexOf(current) !== -1 && current.length > 1){
+    if ($this.attr('href').indexOf(current) !== -1 && current.length > 1) {
       $this.addClass('active');
     }
-  })
+  });
 });
