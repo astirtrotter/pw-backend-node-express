d8a953f61dd3a100bdcee7de6e2f2955cb9ac0f6
prodev9999 <prodev9999@gmail.com>
2019-07-18 15:28:00 +0200

    Minor bug fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,9 +5,9 @@ exports.requireLogin = (req, res, next) => {
   next();
 };
 
-exports.requireAdmin = [requireLogin, (req, res, next) => {
+exports.requireAdmin = (req, res, next) => {
   if (!req.user.meta.admin) {
     return next(res.error(401, 'You are not allowed'))
   }
   next();
-}];
+};
