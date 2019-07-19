3fb304f7236d65eda9458376b7824ff4e20ab033
prodev9999 <prodev9999@gmail.com>
2019-07-19 14:01:05 +0200

    Updated table row clickable

--- public/js/sites.js
+++ public/js/sites.js
@@ -1,6 +1,6 @@
 $(function () {
   let current = location.pathname;
-  $('ul li a').each(function () {
+  $('nav ul li a').each(function () {
     let $this = $(this);
     // if the current path is like this link, make it active
     if (current.startsWith($this.attr('href')) && current.length > 1) {
@@ -8,8 +8,10 @@ $(function () {
     }
   });
 
-  $('*[data-href]').on('click', function() {
-    var win = window.open($(this).data("href"), '_blank');
-    win.focus();
+  $('[data-href]').each(function (i, e) {
+    let href = $(this).data("href");
+    $(e).children('td:not(:last)').click(function () {
+      window.open(href, '_blank');
+    });
   });
 });
