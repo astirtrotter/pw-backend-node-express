6f15b61e7a9f9be61fb0072b98b8810d1929551c
prodev9999 <prodev9999@gmail.com>
2019-07-30 00:23:03 +0800

    Updated skill model

--- models/skill.js
+++ models/skill.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   name: {type: String, unique: true, required: true, trim: true, match: /^[\w\s.-]+$/},
   image: {type: String, required: true, trim: true},
-  combinedSkills: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Skills'
-  }]
-}, {
-  timestamps: true
+  type: {type: String, enum: ['Language', 'Library/Framework', 'Platform', 'Tool', 'Database'], required: true}
 });
 
 const model = mongoose.model('Skills', schema);
