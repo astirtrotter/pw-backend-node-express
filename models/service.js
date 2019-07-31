9cbe514979294bd904f21f2732e3e0abb276a55f
Pro Dev <prodev9999@gmail.com>
2019-07-31 19:49:34 +0800

    Updated service model

--- models/service.js
+++ models/service.js
@@ -2,8 +2,30 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 
 const schema = new mongoose.Schema({
-  name: {type: String, unique: true, required: true, trim: true, match: /^[\w\s.-]+$/},
-  description: {type: String, required: true}
+  header: {
+    title: {type: String, unique: true, required: true, trim: true, match: /^[\w\s.-]+$/},
+    description: {type: String, required: true, trim: true}
+  },
+  description: {
+    //image: /assets/services/:id/description
+    title: String,
+    subtitle: String,
+    overview: String
+  },
+  workflow: {
+    // image: /assets/services/:id/workflow
+    parts: [{
+      title: String,
+      subtitle: String,
+      description: String
+    }],
+    steps: [String]
+  },
+  mentalities: [{
+    // image: /assets/services/:id/mentality_title
+    title: String,
+    description: String
+  }]
 });
 
 schema.post('remove', function () {
